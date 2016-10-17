b815c454a7d45b2faa85bbdf62277ffb360461ef
Rail Sabirov <railich@bk.ru>
2016-10-17 21:13:02 +0300

    Add vizit page

--- app.rb
+++ app.rb
@@ -6,6 +6,16 @@ require 'sinatra/activerecord'
 
 set :database, 'sqlite3:barbershop.db'
 
+helpers do
+  def selected(id, value)
+    'selected' if id.to_s == value.to_s
+  end
+end
+
+before do
+  @barbers = Barber.all
+end
+
 class Client < ActiveRecord::Base
 end
 
@@ -14,6 +24,30 @@ class Barber < ActiveRecord::Base
 end
 
 get '/' do
-  @barbers = Barber.all
 	erb :index
+end
+
+get '/vizit' do
+  erb :vizit
+end
+
+post '/vizit' do
+  @name = params[:name]
+  @phone = params[:phone]
+  @datestamp = params[:datetime]
+  @barber = params[:master]
+  @color = params[:head_color]
+
+  b = Client.new
+  b.name = @name
+  b.phone = @phone
+  b.datestamp = @datestamp
+  b.barber = @barber
+  b.color = @color
+
+  b.save
+
+  @message = "#{@name} вы записаны на #{@datestamp}, на стрижку к #{Barber.find(@barber).name}!"
+
+  erb :vizit
 end
